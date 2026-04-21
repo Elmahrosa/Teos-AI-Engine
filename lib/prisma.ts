774d448bde50ac1d5a599028758570d3db2b080d
Ayman <ams1969@gmail.com>
2026-04-21 14:35:00 +0200

    fix prisma 7 constructor + final launch

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,14 +4,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  // Prisma 7 fix
-  adapter: undefined, // or use Prisma Accelerate if you want
-  log: ['error', 'warn'],
-});
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
